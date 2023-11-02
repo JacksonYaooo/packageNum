21deda60deb63b04f78aac55f5d7731cdf0d9714
huangmengyao <2594838054@qq.com>
2023-11-02 23:43:36 +0800

    feat: 添加li样式

--- src/components/packageNum.vue
+++ src/components/packageNum.vue
@@ -16,6 +16,7 @@ watch(() => props.value, (newValue, oldValue) => {
 <template>
   <ul>
     <li v-for="(item) in todoArr" :key="Math.random()">
+      <input type="checkbox" class="checkBox">
       <span>{{ item }}</span>
       <button class="back">删除</button>
     </li>
@@ -23,5 +24,36 @@ watch(() => props.value, (newValue, oldValue) => {
 </template>
 
 <style scoped>
-li {}
-</style>
+ul {
+  width: 72%;
+  margin: 0 auto;
+  margin-top: 24px;
+  padding: 0;
+}
+
+.checkBox {
+  margin-left: 0px;
+}
+
+li {
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  list-style: none;
+  padding: 0px;
+  margin: 24px 0px;
+}
+
+span {
+  flex: 1;
+  text-align: left;
+  padding: 2px 16px;
+  border-bottom: 1px solid #e18fe3;
+}
+
+button {
+  margin-right: 12px;
+  background-color: rgb(245, 143, 143);
+  color: rgb(80, 41, 41);
+}</style>
